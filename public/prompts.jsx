934ae01456a59ee3bfb7209123897fef30fc2383
codertray <blindguy1992@gmail.com>
2024-01-29 13:41:00 -0700

    initial commit

---
+++ public/prompts.jsx
@@ -0,0 +1,30 @@
+const prompts = {
+    gratitude: [
+        <h3 className="prompt">list 10 things you're grateful for.</h3>,
+        <h3 className="prompt">think of something you're grateful for. why are you grateful for it? how does it make you feel?</h3>,
+        <h3 className="prompt">what's something that made your day easier?</h3>,
+        <h3 className="prompt">Reflect on someone who has positively influenced your life.</h3>,
+        <h3 className="prompt">Describe a small moment of joy or happiness you experienced today.</h3>,
+        <h3 className="prompt">What is something you appreciate about your surroundings or environment?</h3>,
+        <h3 className="prompt">Think about a challenge you've overcome recently and what you learned from it.</h3>
+    ],
+    health: [
+        <h3 className="prompt"> from 1-10 how healthy do you feel today?</h3>,
+        <h3 className="prompt">how have your energy levels feeling today?</h3>,
+        <h3 className="prompt">talk about steps you're taking for self care.</h3>,
+        <h3 className="prompt">How did you prioritize your physical health today?</h3>,
+        <h3 className="prompt">Reflect on a healthy meal or snack you enjoyed today.</h3>,
+        <h3 className="prompt">Describe a physical activity or exercise that made you feel good.</h3>,
+        <h3 className="prompt">What is one thing you can do tomorrow to improve your overall well-being?</h3>
+    ],
+    theme: [
+        <h3 className="prompt">how empowered do you feel today?</h3>,
+        <h3 className="prompt">what steps are you taking to feel more empowered?</h3>,
+        <h3 className="prompt">Reflect on a recent accomplishment or success and how it made you feel.</h3>,
+        <h3 className="prompt">Describe a goal you're working towards and the progress you've made.</h3>,
+        <h3 className="prompt">How do you envision your ideal self and what steps can you take to get closer to that vision?</h3>,
+        <h3 className="prompt">Think about a role model or mentor who inspires you to be your best self.</h3>
+    ]
+};
+
+export default prompts;
